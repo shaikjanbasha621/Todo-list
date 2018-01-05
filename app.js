13e6cefcf6d9948ae139509d54da07af93e7faa2
shaikjanbashsa621 <shaikjanbasha621@gmail.com>
2018-01-05 10:49:21 +0530

    2nd commit

--- app.js
+++ app.js
@@ -14,12 +14,10 @@ var app = express();
 
 var jan,arif;
 
-// view engine setup
+// view engine setup this is basha
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
